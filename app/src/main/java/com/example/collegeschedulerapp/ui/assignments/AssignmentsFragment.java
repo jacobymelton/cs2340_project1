571441b38df8b590371d9dd2afdc8a3caf08e1c5
jacobymelton <118556877+jacobymelton@users.noreply.github.com>
2024-01-25 14:56:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/collegeschedulerapp/ui/assignments/AssignmentsFragment.java
@@ -0,0 +1,23 @@
+package com.example.collegeschedulerapp.ui.assignments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.collegeschedulerapp.R;
+
+public class AssignmentsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_assignments, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/collegeschedulerapp/ui/newtab/NewTabActivity.java
@@ -0,0 +1,16 @@
+package com.example.collegeschedulerapp.ui.newtab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.collegeschedulerapp.R;
+
+public class NewTabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newtab);
+    }
+}
